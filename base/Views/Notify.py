6c1a055a85a588b8a89254f03bd16f1907d9a735
NagiPragalathan <nagipragalathan@gmail.com>
2024-03-22 17:35:12 +0530

    notificatrion backend are updated

--- base/Views/Notify.py
+++ base/Views/Notify.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render, redirect
 from base.models import Notification
+from django.utils import timezone
 
 def add_notification(request):
+    datas = Notification.objects.filter(user=request.user).order_by('-last_updated_time')
     if request.method == 'POST':
-        return render(request, 'add_notification.html')
+        notify_time = request.POST.get('notify_time')
+        tablet_name = request.POST.get('tablet_name')
+        quantity = request.POST.get('quantity')
+        total_tablet_quantity = request.POST.get('total_tablet_quantity')
+
+        # Create a new Notification object and save it to the database
+        Notification.objects.create(
+            user=request.user,
+            notify_time=notify_time,
+            tablet_name=tablet_name,
+            quantity=quantity,
+            total_tablet_quantity=total_tablet_quantity,
+            last_updated_time=timezone.now()
+        )
+        
+        return redirect('notification_list')  # Redirect to notification list view
+    return render(request, 'Notify/add_notification.html', {'datas':datas})
