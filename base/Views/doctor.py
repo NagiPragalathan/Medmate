6d70eddadade3e2bff0efc9c63647eeed5c86bc9
NagiPragalathan <nagipragalathan@gmail.com>
2024-04-25 16:32:36 +0530

    Hardware are connected

--- base/Views/doctor.py
+++ base/Views/doctor.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from base.models import UserRole
+from base.models import UserRole, UserProfile
 
 def DoctorList(request):
     usr_obj = UserRole.objects.filter(role='doctor')
     users = []
     for i in usr_obj:
-        users.append(User.objects.get(id=i.user.id))
+        user_temp = User.objects.get(id=i.user.id)
+        user_temp.profile = UserProfile(user=user_temp)
+        users.append(user_temp)
         print(i.user, i.role, i)
     print(users)
-    return render(request, 'doctor/list-Doctor.html')
+    return render(request, 'doctor/list-Doctor.html', { "doctors" : users })
