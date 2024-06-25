9b324dd1cc586d97526c37093388bc9d4d66d3d4
NagiPragalathan <nagipragalathan@gmail.com>
2024-06-25 15:38:19 +0530

    "Added echo statements and duplicated pip install command in build_files.sh"

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,5 @@
+echo "starting install..."
+pip install -r requirements.txt
 python3.12 -m pip install -r requirements.txt
 python3.12 manage.py collectstatic
+echo "end install-----------------------"
