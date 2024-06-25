0ad9aa902eab05ba1a15f533bb3d8c7665a253a6
NagiPragalathan <nagipragalathan@gmail.com>
2024-06-25 15:17:58 +0530

    "Updated Python bytecode files and modified build_files.sh script"

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Collect static files
-python3.9 manage.py collectstatic --noinput
-
-# Apply database migrations
-python3.9 manage.py migrate
+python3.12 -m pip install -r requirements.txt
+python3.12 manage.py collectstatic
